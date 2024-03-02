d7b36fde41cd680c0ed1a7641323c08a534e0da9
Zachery <xacheri@outlook.com>
2024-03-01 22:37:14 -0600

    The First Great Repo Culling

--- PantryShareApp/PantryShareApp/vite.config.js
+++ PantryShareApp/PantryShareApp/vite.config.js
@@ -1,10 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),  VitePWA({
-    registerType: 'script-defer'
-  })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
