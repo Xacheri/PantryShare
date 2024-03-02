2e8d131e483f23b08c191cb122b1c0275b40fb3a
Zachery <xacheri@outlook.com>
2024-03-02 13:17:00 -0600

    nav menu animation

--- PantryShareApp/PantryShareApp/src/App.vue
+++ PantryShareApp/PantryShareApp/src/App.vue
@@ -1,32 +1,34 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 const menuClicked = ref(false);
+const navStyle = ref("")
 
-const toggleMenu = function() {
+const toggleMenu = function () {
   menuClicked.value = !menuClicked.value;
+  navStyle.value = menuClicked.value ? "fade-inflate-fast" : "fade-deflate-fast";
 }
 </script>
 
 <template>
   <header class="d-flex flex-column align-items-center">
-    <img src="@/assets/logo.png" @click="toggleMenu" alt="logo" width="150"/>
-    <nav v-if="menuClicked" class="w-75">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
-      <RouterLink to="/register">Register</RouterLink>
-      <RouterLink to="/search">Search</RouterLink>
-      <RouterLink to="/stockthepantry">Stock the Pantry</RouterLink>
-      <RouterLink to="/makerequest">Make Request</RouterLink>
-      <RouterLink to="/servicedetails">Service Details</RouterLink>
-    </nav>
+    <img src="@/assets/logo.png" @click="toggleMenu" alt="logo" :width="menuClicked ? 75 : 150" />
+    <transition name="fade">
+      <nav v-if="menuClicked" class="w-75" :class="navStyle">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/register">Register</RouterLink>
+        <RouterLink to="/search">Search</RouterLink>
+        <RouterLink to="/stockthepantry">Stock the Pantry</RouterLink>
+        <RouterLink to="/makerequest">Make Request</RouterLink>
+        <RouterLink to="/servicedetails">Service Details</RouterLink>
+      </nav>
+    </transition>
   </header>
   <RouterView />
   <footer>
-
   </footer>
 </template>
 
@@ -40,5 +42,12 @@ nav {
   padding: 10px;
   border-radius: 10px;
   margin-top: 10px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s, transform 1s; /* Transition opacity and transform */
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
