23857c3e1b788ea506692f42b046680e44a50696
Zachery <xacheri@outlook.com>
2024-03-02 12:25:30 -0600

    app landing page structure

--- PantryShareApp/PantryShareApp/src/App.vue
+++ PantryShareApp/PantryShareApp/src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue'
+
+const menuClicked = ref(false);
+
+const toggleMenu = function() {
+  menuClicked.value = !menuClicked.value;
+}
 </script>
 
 <template>
-  <header>
-    <nav>
+  <header class="d-flex flex-column align-items-center">
+    <img src="@/assets/logo.png" @click="toggleMenu" alt="logo" width="150"/>
+    <nav v-if="menuClicked" class="w-75">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/login">Login</RouterLink>
@@ -15,74 +23,22 @@ import HelloWorld from './components/HelloWorld.vue'
       <RouterLink to="/makerequest">Make Request</RouterLink>
       <RouterLink to="/servicedetails">Service Details</RouterLink>
     </nav>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
-
   <RouterView />
+  <footer>
+
+  </footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-radius: 10px;
+  margin-top: 10px;
 }
 </style>
